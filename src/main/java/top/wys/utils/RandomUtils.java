a91ac291eeadc199cd4c56e5ab53685391a6375f
zml <1072307340>
2019-08-28 23:45:44 +0800

    添加随机生成身份证号的方法

--- src/main/java/top/wys/utils/RandomUtils.java
+++ src/main/java/top/wys/utils/RandomUtils.java
@@ -3,6 +3,9 @@ package top.wys.utils;
  * Created by 郑明亮 on 2018/10/1 15:58.
  */
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -53,6 +56,15 @@ public class RandomUtils {
             "Dalvik/2.1.0 (Linux; U; Android 7.0; BTV-W09 Build/HUAWEIBEETHOVEN-W09)"};
     private static final Random RANDOM = new Random();
 
+    /**
+     *  随机生成省、自治区、直辖市代码 1-2
+     */
+    private static final String PROVINCES[] = { "11", "12", "13", "14", "15", "21", "22", "23",
+            "31", "32", "33", "34", "35", "36", "37", "41", "42", "43",
+            "44", "45", "46", "50", "51", "52", "53", "54", "61", "62",
+            "63", "64", "65", "71", "81", "82" };
+
+    private static final String ID_CARD_SEX_CHECK_NUMS[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","X" };
     /**
      * 获取随机ip地址
      *
@@ -124,6 +136,96 @@ public class RandomUtils {
         }
         sb.append("科技有限公司");
         return sb.toString();
+    }
+
+    /**
+     * 获取随机身份证号
+     * @return
+     */
+    public static String getRandomIdCard() {
+        String id = "";
+
+        // 随机生成省、自治区、直辖市代码 1-2
+        String province = randomOne(PROVINCES);
+        // 随机生成地级市、盟、自治州代码 3-4
+        String city = randomCityCode(18);
+        // 随机生成县、县级市、区代码 5-6
+        String county = randomCityCode(28);
+        // 随机生成出生年月 7-14
+        String birth = randomBirth(20, 50);
+        // 随机生成顺序号 15-17(随机性别)
+        String no = RANDOM.nextInt(899) + 100+"";
+        // 随机生成校验码 18
+
+        String check = randomOne(ID_CARD_SEX_CHECK_NUMS);
+        // 拼接身份证号码
+        id = province + city + county + birth + no + check;
+        return id;
+    }
+
+    /**
+     * 获取随机身份证号
+     * @return
+     */
+    public static String getRandomIdCard(int minAge,int maxAge) {
+        String id = "";
+
+        // 随机生成省、自治区、直辖市代码 1-2
+        String province = randomOne(PROVINCES);
+        // 随机生成地级市、盟、自治州代码 3-4
+        String city = randomCityCode(18);
+        // 随机生成县、县级市、区代码 5-6
+        String county = randomCityCode(28);
+        // 随机生成出生年月 7-14
+        String birth = randomBirth(minAge, maxAge);
+        // 随机生成顺序号 15-17(随机性别)
+        String no = RANDOM.nextInt(899) + 100+"";
+        // 随机生成校验码 18
+
+        String check = randomOne(ID_CARD_SEX_CHECK_NUMS);
+        // 拼接身份证号码
+        id = province + city + county + birth + no + check;
+        return id;
+    }
+
+
+    /**
+     * 从String[] 数组中随机取出其中一个String字符串
+     *
+     * @param randomArray
+     * @return
+     */
+    public static String randomOne(String randomArray[]) {
+        return randomArray[RANDOM.nextInt(randomArray.length - 1)];
+    }
+
+    /**
+     * 随机生成两位数的字符串（01-max）,不足两位的前面补0
+     *
+     * @param max 最大值
+     * @return
+     */
+    private static String randomCityCode(int max) {
+        int i = RANDOM.nextInt(max) + 1;
+        return i > 9 ? i + "" : "0" + i;
+    }
+
+    /**
+     * 随机生成minAge到maxAge年龄段的人的生日日期
+     *
+     * @param minAge 最小年龄
+     * @param maxAge 最大年龄
+     * @return
+     */
+    private static String randomBirth(int minAge, int maxAge) {
+        SimpleDateFormat dft = new SimpleDateFormat("yyyyMMdd");// 设置日期格式
+        Calendar date = Calendar.getInstance();
+        date.setTime(new Date());// 设置当前日期
+        // 随机设置日期为前maxAge年到前minAge年的任意一天
+        int randomDay = 365 * minAge
+                + RANDOM.nextInt(365 * (maxAge - minAge));
+        date.set(Calendar.DATE, date.get(Calendar.DATE) - randomDay);
+        return dft.format(date.getTime());
     }
 
     /**
