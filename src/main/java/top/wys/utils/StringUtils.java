01135da72ed64d7b0df12d36c4a27c01541ffc7d
zhengmingliang <1072307340@qq.com>
2021-11-28 22:33:31 +0800

    添加部分注释，添加部分新增方法

--- src/main/java/top/wys/utils/StringUtils.java
+++ src/main/java/top/wys/utils/StringUtils.java
@@ -1,8 +1,17 @@
 package top.wys.utils;
 
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+
 import java.io.UnsupportedEncodingException;
 import java.text.DecimalFormat;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,72 +22,85 @@ import java.util.regex.Pattern;
  */
 public class StringUtils {
 
+
+    private static final String FOLDER_SEPARATOR = "/";
+
+    private static final String WINDOWS_FOLDER_SEPARATOR = "\\";
+
+    private static final String TOP_PATH = "..";
+
+    private static final String CURRENT_PATH = ".";
+
+    private static final char EXTENSION_SEPARATOR = '.';
+
+
+
     private StringUtils() {
         throw new UnsupportedOperationException("Can not be instantiated...");
     }
 
-/**
- * @author 郑明亮
- * @time 2017年1月23日 上午9:58:07
- * @description <p> 将Unicode编码的文本进行解码</p>
- * @param str Unicode编码文本
- * @return 解码后的文本
- */
-public static String unicodeToString(String str) {
+    /**
+     * @param str Unicode编码文本
+     * @return 解码后的文本
+     * @author 郑明亮
+     * @time 2017年1月23日 上午9:58:07
+     * @description <p> 将Unicode编码的文本进行解码</p>
+     */
+    public static String unicodeToString(String str) {
 
-    Pattern pattern = Pattern.compile("(\\\\u(\\p{XDigit}{4}))");
-    Matcher matcher = pattern.matcher(str);
-    char ch;
-    while (matcher.find()) {
-        ch = (char) Integer.parseInt(matcher.group(2), 16);
-        str = str.replace(matcher.group(1), ch + "");
-    }
-    return str;
-}
-
-/**
- * @author 郑明亮
- * @time 2017年1月23日 上午9:57:20
- * @description <p>将中文转换为Unicode编码   </p>
- * @param s 想要进行编码的文本
- * @return Unicode编码 文本
- */
-public static String getUnicode(String s) {
-    try {
-        StringBuffer out = new StringBuffer("");
-        byte[] bytes = s.getBytes("unicode");
-        for (int i = 0; i < bytes.length - 1; i += 2) {
-            out.append("\\u");
-            String str = Integer.toHexString(bytes[i + 1] & 0xff);
-            for (int j = str.length(); j < 2; j++) {
-                out.append("0");
-            }
-            String str1 = Integer.toHexString(bytes[i] & 0xff);
-            out.append(str1);
-            out.append(str);
-
+        Pattern pattern = Pattern.compile("(\\\\u(\\p{XDigit}{4}))");
+        Matcher matcher = pattern.matcher(str);
+        char ch;
+        while (matcher.find()) {
+            ch = (char) Integer.parseInt(matcher.group(2), 16);
+            str = str.replace(matcher.group(1), ch + "");
         }
-        return out.toString();
-    } catch (UnsupportedEncodingException e) {
-        e.printStackTrace();
-        return null;
+        return str;
     }
-}
 
-/**
- * 将 inStr 转为 UTF-8 的编码形式
- *
- * @param inStr 输入字符串
- * @return UTF - 8 的编码形式的字符串
- * @throws UnsupportedEncodingException
- */
-public static String ISO2UTF(String inStr) throws UnsupportedEncodingException {
-    String outStr = "";
-    if (inStr != null) {
-        outStr = new String(inStr.getBytes("iso-8859-1"), "UTF-8");
+    /**
+     * @param s 想要进行编码的文本
+     * @return Unicode编码 文本
+     * @author 郑明亮
+     * @time 2017年1月23日 上午9:57:20
+     * @description <p>将中文转换为Unicode编码   </p>
+     */
+    public static String getUnicode(String s) {
+        try {
+            StringBuffer out = new StringBuffer("");
+            byte[] bytes = s.getBytes("unicode");
+            for (int i = 0; i < bytes.length - 1; i += 2) {
+                out.append("\\u");
+                String str = Integer.toHexString(bytes[i + 1] & 0xff);
+                for (int j = str.length(); j < 2; j++) {
+                    out.append("0");
+                }
+                String str1 = Integer.toHexString(bytes[i] & 0xff);
+                out.append(str1);
+                out.append(str);
+
+            }
+            return out.toString();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
-    return outStr;
-}
+
+    /**
+     * 将 inStr 转为 UTF-8 的编码形式
+     *
+     * @param inStr 输入字符串
+     * @return UTF - 8 的编码形式的字符串
+     * @throws UnsupportedEncodingException
+     */
+    public static String ISO2UTF(String inStr) throws UnsupportedEncodingException {
+        String outStr = "";
+        if (inStr != null) {
+            outStr = new String(inStr.getBytes("iso-8859-1"), "UTF-8");
+        }
+        return outStr;
+    }
 
     /**
      * 判断字符串是否为null或长度为0
@@ -91,11 +113,11 @@ public static String ISO2UTF(String inStr) throws UnsupportedEncodingException {
     }
 
     /**
+     * @param s 待校验字符串
+     * @return {@code false}: 空 {@code true}: 不为空
      * @author 郑明亮
      * @time 2017年2月6日 上午11:13:48
      * @description <p> 判断字符串是否不为null或长度大于0  </p>
-     * @param s 待校验字符串
-     * @return {@code false}: 空 {@code true}: 不为空
      */
     public static boolean isNotEmpty(CharSequence s) {
         return !isEmpty(s);
@@ -251,60 +273,310 @@ public static String ISO2UTF(String inStr) throws UnsupportedEncodingException {
     }
 
     /**
+     * @param amountString 浮点型金额字符串
+     * @return 整型数字符串
      * @author 郑明亮
      * @time 2017年1月12日 下午1:16:39
      * @description <p>去掉浮点类型的小数点，并进行四舍五入   </p>
-     *              <p>应用场景：将浮点型转换为整型数据，并保留1位小数</p>
-     * @param amountString 浮点型金额字符串
-     * @return 整型数字符串
+     * <p>应用场景：将浮点型转换为整型数据，并保留1位小数</p>
      */
-    public static String removeAmountPoint(String amountString){
+    public static String removeAmountPoint(String amountString) {
         String noPointString = "";
         if (amountString == null) {
             return noPointString;
-        }else{
+        } else {
             Integer amount;
-            if (amountString.indexOf("." )!= -1) {
-                String pointNum =  amountString.substring(amountString.indexOf("." ));
-                amount  = Integer.parseInt(amountString.replace(pointNum, ""));
-                if (pointNum.length()>2) {
+            if (amountString.indexOf(".") != -1) {
+                String pointNum = amountString.substring(amountString.indexOf("."));
+                amount = Integer.parseInt(amountString.replace(pointNum, ""));
+                if (pointNum.length() > 2) {
                     int num = Integer.parseInt(pointNum.substring(1, 2));
-                    if (num >=5) {
-                        amount ++;
+                    if (num >= 5) {
+                        amount++;
 
                     }
                 }
-                noPointString = ""+amount;
-            }else {
+                noPointString = "" + amount;
+            } else {
                 return amountString;
             }
             return noPointString;
         }
 
     }
-/**
-     * @author 郑明亮
-     * @time 2017年1月13日 下午4:18:14
-     * @param amount 金额
+
+    /**
+     * @param amount  金额
      * @param pattern 将数字转换为指定格式
-     * <p>如 amount： 10000000  ，当传入金额为null时，默认为0 </p>
-     * <p>如 pattern：  \u00A5,###.00 ，当pattern为null时，使用默认pattern ,即"\u00A5,##0.00" </p>
-     * <p>如 return： ¥10,000,000   </p>
+     *                <p>如 amount： 10000000  ，当传入金额为null时，默认为0 </p>
+     *                <p>如 pattern：  \u00A5,###.00 ，当pattern为null时，使用默认pattern ,即"\u00A5,##0.00" </p>
+     *                <p>如 return： ¥10,000,000   </p>
      * @return 格式化后的数字字符串
      * @throws ParseException
+     * @author 郑明亮
+     * @time 2017年1月13日 下午4:18:14
      */
-    public static String formateAmount(Object amount,String pattern) throws ParseException{
+    public static String formateAmount(Object amount, String pattern) throws ParseException {
         if (amount == null) {//当传入金额为null时，默认为0
-            return formateAmount(0,pattern);
+            return formateAmount(0, pattern);
         }
         if (pattern == null) {//当pattern为null时，使用默认pattern
             pattern = "\u00A5,##0.00";
         }
         if (amount instanceof String) {
-            return formateAmount(Double.parseDouble(amount.toString()),pattern);
+            return formateAmount(Double.parseDouble(amount.toString()), pattern);
         }
         DecimalFormat format = new DecimalFormat(pattern);
         String number = format.format(amount);
         return number;
+    }
+
+    public static boolean hasLength(CharSequence str) {
+        return (str != null && str.length() > 0);
+    }
+
+    /**
+     * Replace all occurrences of a substring within a string with
+     * another string.
+     * @param inString {@code String} to examine
+     * @param oldPattern {@code String} to replace
+     * @param newPattern {@code String} to insert
+     * @return a {@code String} with the replacements
+     */
+    public static String replace(String inString, String oldPattern, String newPattern) {
+        if (!hasLength(inString) || !hasLength(oldPattern) || newPattern == null) {
+            return inString;
+        }
+        StringBuilder sb = new StringBuilder();
+        int pos = 0; // our position in the old string
+        int index = inString.indexOf(oldPattern);
+        // the index of an occurrence we've found, or -1
+        int patLen = oldPattern.length();
+        while (index >= 0) {
+            sb.append(inString.substring(pos, index));
+            sb.append(newPattern);
+            pos = index + patLen;
+            index = inString.indexOf(oldPattern, pos);
+        }
+        sb.append(inString.substring(pos));
+        // remember to append any characters to the right of a match
+        return sb.toString();
+    }
+
+    /**
+     * Normalize the path by suppressing sequences like "path/.." and
+     * inner simple dots.
+     * <p>The result is convenient for path comparison. For other uses,
+     * notice that Windows separators ("\") are replaced by simple slashes.
+     * @param path the original path
+     * @return the normalized path
+     */
+    public static String cleanPath(String path) {
+        if (path == null) {
+            return null;
+        }
+        String pathToUse = replace(path, WINDOWS_FOLDER_SEPARATOR, FOLDER_SEPARATOR);
+
+        // Strip prefix from path to analyze, to not treat it as part of the
+        // first path element. This is necessary to correctly parse paths like
+        // "file:core/../core/io/Resource.class", where the ".." should just
+        // strip the first "core" directory while keeping the "file:" prefix.
+        int prefixIndex = pathToUse.indexOf(":");
+        String prefix = "";
+        if (prefixIndex != -1) {
+            prefix = pathToUse.substring(0, prefixIndex + 1);
+            if (prefix.contains("/")) {
+                prefix = "";
+            }
+            else {
+                pathToUse = pathToUse.substring(prefixIndex + 1);
+            }
+        }
+        if (pathToUse.startsWith(FOLDER_SEPARATOR)) {
+            prefix = prefix + FOLDER_SEPARATOR;
+            pathToUse = pathToUse.substring(1);
+        }
+
+        String[] pathArray = delimitedListToStringArray(pathToUse, FOLDER_SEPARATOR);
+        List<String> pathElements = new LinkedList<String>();
+        int tops = 0;
+
+        for (int i = pathArray.length - 1; i >= 0; i--) {
+            String element = pathArray[i];
+            if (CURRENT_PATH.equals(element)) {
+                // Points to current directory - drop it.
+            }
+            else if (TOP_PATH.equals(element)) {
+                // Registering top path found.
+                tops++;
+            }
+            else {
+                if (tops > 0) {
+                    // Merging path element with element corresponding to top path.
+                    tops--;
+                }
+                else {
+                    // Normal path element found.
+                    pathElements.add(0, element);
+                }
+            }
+        }
+
+        // Remaining top paths need to be retained.
+        for (int i = 0; i < tops; i++) {
+            pathElements.add(0, TOP_PATH);
+        }
+
+        return prefix + collectionToDelimitedString(pathElements, FOLDER_SEPARATOR);
+    }
+
+    /**
+     * Compare two paths after normalization of them.
+     * @param path1 first path for comparison
+     * @param path2 second path for comparison
+     * @return whether the two paths are equivalent after normalization
+     */
+    public static boolean pathEquals(String path1, String path2) {
+        return cleanPath(path1).equals(cleanPath(path2));
+    }
+
+
+    /**
+     * Delete any character in a given {@code String}.
+     * @param inString the original {@code String}
+     * @param charsToDelete a set of characters to delete.
+     * E.g. "az\n" will delete 'a's, 'z's and new lines.
+     * @return the resulting {@code String}
+     */
+    public static String deleteAny(String inString, String charsToDelete) {
+        if (!hasLength(inString) || !hasLength(charsToDelete)) {
+            return inString;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < inString.length(); i++) {
+            char c = inString.charAt(i);
+            if (charsToDelete.indexOf(c) == -1) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Turn given source {@code String} array into sorted array.
+     * @param array the source array
+     * @return the sorted array (never {@code null})
+     */
+    public static String[] sortStringArray(String[] array) {
+        if (ObjectUtils.isEmpty(array)) {
+            return new String[0];
+        }
+        Arrays.sort(array);
+        return array;
+    }
+
+    /**
+     * Copy the given {@code Collection} into a {@code String} array.
+     * <p>The {@code Collection} must contain {@code String} elements only.
+     * @param collection the {@code Collection} to copy
+     * @return the {@code String} array ({@code null} if the supplied
+     * {@code Collection} was {@code null})
+     */
+    public static String[] toStringArray(Collection<String> collection) {
+        if (collection == null) {
+            return null;
+        }
+        return collection.toArray(new String[collection.size()]);
+    }
+
+    /**
+     * Take a {@code String} that is a delimited list and convert it into a
+     * {@code String} array.
+     * <p>A single {@code delimiter} may consist of more than one character,
+     * but it will still be considered as a single delimiter string, rather
+     * than as bunch of potential delimiter characters, in contrast to
+     * @param str the input {@code String}
+     * @param delimiter the delimiter between elements (this is a single delimiter,
+     * rather than a bunch individual delimiter characters)
+     * @return an array of the tokens in the list
+     */
+    public static String[] delimitedListToStringArray(String str, String delimiter) {
+        return delimitedListToStringArray(str, delimiter, null);
+    }
+
+    /**
+     * Take a {@code String} that is a delimited list and convert it into
+     * a {@code String} array.
+     * <p>A single {@code delimiter} may consist of more than one character,
+     * but it will still be considered as a single delimiter string, rather
+     * than as bunch of potential delimiter characters, in contrast to
+     * @param str the input {@code String}
+     * @param delimiter the delimiter between elements (this is a single delimiter,
+     * rather than a bunch individual delimiter characters)
+     * @param charsToDelete a set of characters to delete; useful for deleting unwanted
+     * line breaks: e.g. "\r\n\f" will delete all new lines and line feeds in a {@code String}
+     * @return an array of the tokens in the list
+     */
+    public static String[] delimitedListToStringArray(String str, String delimiter, String charsToDelete) {
+        if (str == null) {
+            return new String[0];
+        }
+        if (delimiter == null) {
+            return new String[] {str};
+        }
+        List<String> result = new ArrayList<String>();
+        if ("".equals(delimiter)) {
+            for (int i = 0; i < str.length(); i++) {
+                result.add(deleteAny(str.substring(i, i + 1), charsToDelete));
+            }
+        }
+        else {
+            int pos = 0;
+            int delPos;
+            while ((delPos = str.indexOf(delimiter, pos)) != -1) {
+                result.add(deleteAny(str.substring(pos, delPos), charsToDelete));
+                pos = delPos + delimiter.length();
+            }
+            if (str.length() > 0 && pos <= str.length()) {
+                // Add rest of String, but not in case of empty input.
+                result.add(deleteAny(str.substring(pos), charsToDelete));
+            }
+        }
+        return toStringArray(result);
+    }
+
+    /**
+     * 将 {@link Collection} 转换为有范围的{@code String} (e.g. CSV).
+     * <p>对于{@code toString()} 的实现很有用.
+     * @param coll 要转换的 {@code Collection}
+     * @param delim 分隔符 (一般用 ",")
+     * @param prefix 每个元素的前缀 {@code String}
+     * @param suffix 每个元素的后缀 {@code String}
+     * @return the delimited {@code String}
+     */
+    public static String collectionToDelimitedString(Collection<?> coll, String delim, String prefix, String suffix) {
+        if (CollectionUtils.isEmpty(coll)) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        Iterator<?> it = coll.iterator();
+        while (it.hasNext()) {
+            sb.append(prefix).append(it.next()).append(suffix);
+            if (it.hasNext()) {
+                sb.append(delim);
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 将 {@link Collection} 转换为有范围的{@code String} (e.g. CSV).
+     * <p>Useful for {@code toString()} implementations.
+     * @param coll the {@code Collection} to convert
+     * @param delim the delimiter to use (typically a ",")
+     * @return the delimited {@code String}
+     */        
+    public static String collectionToDelimitedString(Collection<?> coll, String delim) {
+        return collectionToDelimitedString(coll, delim, "", "");
     }
 }
