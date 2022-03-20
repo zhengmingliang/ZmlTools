72498b4c354689216fc489e2c9448a17ac7fe05a
zhengmingliang <1072307340@qq.com>
2022-03-20 22:44:17 +0800

    添加获取get和set反射方法

--- src/main/java/top/wys/utils/ReflectionUtils.java
+++ src/main/java/top/wys/utils/ReflectionUtils.java
@@ -4,6 +4,10 @@ package top.wys.utils;
 import com.google.common.collect.Lists;
 
 import javax.annotation.Nullable;
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -353,4 +357,41 @@ public class ReflectionUtils {
 //		declaredMethodsCache.clear();
 		declaredFieldsCache.clear();
 	}
+
+	public static PropertyDescriptor[] getBeanProperties(Class type) {
+		return getPropertiesHelper(type, true, true);
+	}
+
+	public static PropertyDescriptor[] getBeanGetters(Class type) {
+		return getPropertiesHelper(type, true, false);
+	}
+
+	public static PropertyDescriptor[] getBeanSetters(Class type) {
+		return getPropertiesHelper(type, false, true);
+	}
+
+	private static PropertyDescriptor[] getPropertiesHelper(Class type, boolean read, boolean write) {
+		try {
+			BeanInfo info = Introspector.getBeanInfo(type, Object.class);
+			PropertyDescriptor[] all = info.getPropertyDescriptors();
+			if (read && write) {
+				return all;
+			}
+			List properties = new ArrayList(all.length);
+			for (int i = 0; i < all.length; i++) {
+				PropertyDescriptor pd = all[i];
+				if ((read && pd.getReadMethod() != null) ||
+						(write && pd.getWriteMethod() != null)) {
+					properties.add(pd);
+				}
+			}
+			return (PropertyDescriptor[]) properties.toArray(new PropertyDescriptor[properties.size()]);
+		}
+		catch (IntrospectionException e) {
+			e.printStackTrace();
+		}
+		return emptyProperty;
+	}
+
+	private static final PropertyDescriptor[] emptyProperty = {};
 }
