47a732dd5e77ecbc6ffcda292f20e0c208f527ad
zhengmingliang <1072307340@qq.com>
2022-03-21 22:16:40 +0800

    添加Class处理工具类

--- src/main/java/top/wys/utils/ClassUtils.java
+++ src/main/java/top/wys/utils/ClassUtils.java
@@ -8,7 +8,7 @@ package top.wys.utils;
 
 /**
  * <ol>
- *
+ * Class处理工具类
  * </ol>
  *
  * @author 郑明亮
@@ -17,4 +17,33 @@ package top.wys.utils;
  * @email mpro@vip.qq.com
  */
 public class ClassUtils {
+    /**
+     * 返回要使用的默认 ClassLoader：通常是线程上下文 ClassLoader（如果可用）；加载 ClassUtils 类的 ClassLoader 将用作后备。
+     * 如果您打算在显然更喜欢非空 ClassLoader 引用的场景中使用线程上下文 ClassLoader，
+     * 请调用此方法：例如，用于类路径资源加载（但不一定适用于 Class.forName，它也接受空类加载器引用）
+     * @return 默认类加载器（仅当系统类加载器不可访问时为空）
+     */
+    public static ClassLoader getDefaultClassLoader() {
+        ClassLoader cl = null;
+        try {
+            cl = Thread.currentThread().getContextClassLoader();
+        }
+        catch (Throwable ex) {
+            // Cannot access thread context ClassLoader - falling back...
+        }
+        if (cl == null) {
+            // No thread context class loader -> use class loader of this class.
+            cl = ClassUtils.class.getClassLoader();
+            if (cl == null) {
+                // getClassLoader() returning null indicates the bootstrap ClassLoader
+                try {
+                    cl = ClassLoader.getSystemClassLoader();
+                }
+                catch (Throwable ex) {
+                    // Cannot access system ClassLoader - oh well, maybe the caller can live with null...
+                }
+            }
+        }
+        return cl;
+    }
 }
