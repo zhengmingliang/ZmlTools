13690c728cfaec31c35c2911aa5b944743574523
zhengmingliang <1072307340@qq.com>
2022-06-25 22:59:47 +0800

    修改过期方法

--- src/main/java/top/wys/utils/entity/Date.java
+++ src/main/java/top/wys/utils/entity/Date.java
@@ -3,7 +3,6 @@
  */
 package top.wys.utils.entity;
 
-import sun.util.calendar.CalendarUtils;
 import top.wys.utils.DateUtils;
 
 import java.util.Calendar;
@@ -49,8 +48,8 @@ public class Date extends java.util.Date {
     public String toDateString(){
         StringBuilder builder = new StringBuilder(10);
         builder.append(calendar.get(Calendar.YEAR)).append('-');
-        CalendarUtils.sprintf0d(builder,calendar.get(Calendar.MONTH) + 1,2).append('-');
-        CalendarUtils.sprintf0d(builder,calendar.get(Calendar.DAY_OF_MONTH),2);
+        sprintf0d(builder,calendar.get(Calendar.MONTH) + 1,2).append('-');
+        sprintf0d(builder,calendar.get(Calendar.DAY_OF_MONTH),2);
        return builder.toString();
     }
 
@@ -61,12 +60,36 @@ public class Date extends java.util.Date {
     public String toDateTimeString(){
         StringBuilder builder = new StringBuilder(19);
         builder.append(calendar.get(Calendar.YEAR)).append('-');
-        CalendarUtils.sprintf0d(builder,calendar.get(Calendar.MONTH) + 1,2).append('-');
-        CalendarUtils.sprintf0d(builder,calendar.get(Calendar.DAY_OF_MONTH),2).append(' ');
-        CalendarUtils.sprintf0d(builder,calendar.get(Calendar.HOUR_OF_DAY),2).append(':');
-        CalendarUtils.sprintf0d(builder,calendar.get(Calendar.MINUTE),2).append(':');
-        CalendarUtils.sprintf0d(builder,calendar.get(Calendar.SECOND),2);
+        sprintf0d(builder,calendar.get(Calendar.MONTH) + 1,2).append('-');
+        sprintf0d(builder,calendar.get(Calendar.DAY_OF_MONTH),2).append(' ');
+        sprintf0d(builder,calendar.get(Calendar.HOUR_OF_DAY),2).append(':');
+        sprintf0d(builder,calendar.get(Calendar.MINUTE),2).append(':');
+        sprintf0d(builder,calendar.get(Calendar.SECOND),2);
         return builder.toString();
+    }
+
+    private static final StringBuilder sprintf0d(StringBuilder var0, int var1, int var2) {
+        long var3 = (long)var1;
+        if (var3 < 0L) {
+            var0.append('-');
+            var3 = -var3;
+            --var2;
+        }
+
+        int var5 = 10;
+
+        int var6;
+        for(var6 = 2; var6 < var2; ++var6) {
+            var5 *= 10;
+        }
+
+        for(var6 = 1; var6 < var2 && var3 < (long)var5; ++var6) {
+            var0.append('0');
+            var5 /= 10;
+        }
+
+        var0.append(var3);
+        return var0;
     }
 
 }
