14c0eccd07314bc4ff86cad660bc6d9b89d7d03d
zhengmingliang <1072307340@qq.com>
2022-12-04 22:33:32 +0800

    获取value时，忽略key的大小写

--- src/main/java/top/wys/utils/collection/MultiKeyHashMap.java
+++ src/main/java/top/wys/utils/collection/MultiKeyHashMap.java
@@ -4,15 +4,42 @@
 package top.wys.utils.collection;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
- * <p> 可以映射多个key到一个value的HashMap实现</p>
+ * <p> 可以映射多个key到一个value的HashMap实现(获取值时可忽略key的大小写，优先获取匹配大小写的值)</p>
  *
  * @author 郑明亮
  * @since  1.3.11
  * @time 2022/3/13 09:27
  */
 public class MultiKeyHashMap<K,V> extends HashMap<K,V> {
+
+    Map<String,K> keyMap = new HashMap<String,K>();
+
+    @Override
+    public V get(Object key) {
+        V v = super.get(key);
+        if(v != null){
+            return v;
+        }
+        K k = keyMap.get(String.valueOf(key).toLowerCase());
+        if (k != null) {
+            return super.get(k);
+        }
+
+        return null;
+    }
+
+
+    @Override
+    public V getOrDefault(Object key, V defaultValue) {
+        V v = get(key);
+        if(v != null){
+            return v;
+        }
+        return defaultValue;
+    }
 
     /**
      * 通过多个key来获取值，返回不为空的key值
@@ -21,11 +48,11 @@ public class MultiKeyHashMap<K,V> extends HashMap<K,V> {
      * @return
      */
     public V get(Object key1,Object key2) {
-        V v = super.get(key1);
+        V v = get(key1);
         if (v != null) {
             return v;
         }
-        return super.get(key2);
+        return get(key2);
     }
 
     /**
@@ -36,17 +63,17 @@ public class MultiKeyHashMap<K,V> extends HashMap<K,V> {
      * @return {@link V}
      */
     public V get(Object key1,Object key2,Object... keys) {
-        V v = super.get(key1);
+        V v = this.get(key1);
         if (v != null) {
             return v;
         }
-        v = super.get(key2);
+        v = this.get(key2);
         if (v != null) {
             return v;
         }
         if (keys != null) {
             for (Object key : keys) {
-                v = super.get(key);
+                v = this.get(key);
                 if (v != null) {
                     return v;
                 }
@@ -56,10 +83,15 @@ public class MultiKeyHashMap<K,V> extends HashMap<K,V> {
         return null;
     }
 
+    @Override
+    public V put(K key, V value) {
+        keyMap.put(String.valueOf(key).toLowerCase(),key);
+        return super.put(key, value);
+    }
 
     public V put(K key, K key2, V value) {
-        super.put(key, value);
-        return super.put(key2,value);
+        put(key, value);
+        return put(key2,value);
     }
 
 
