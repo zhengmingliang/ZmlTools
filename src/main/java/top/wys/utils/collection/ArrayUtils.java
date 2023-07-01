2c231e4aae118b0ad938834ab011637184202eb6
zhengmingliang <zhengmingliang@5ireport.com>
2023-07-01 22:15:36 +0800

    添加deepEqulas0方法

--- src/main/java/top/wys/utils/collection/ArrayUtils.java
+++ src/main/java/top/wys/utils/collection/ArrayUtils.java
@@ -1,6 +1,9 @@
 package top.wys.utils.collection;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static java.util.Arrays.deepEquals;
 
 /**
  * 数组工具类
@@ -58,5 +61,34 @@ public class ArrayUtils {
             return 0;
         }
         return Array.getLength(array);
+    }
+
+
+
+
+    public static boolean deepEquals0(Object e1, Object e2) {
+        assert e1 != null;
+        boolean eq;
+        if (e1 instanceof Object[] && e2 instanceof Object[])
+            eq = deepEquals ((Object[]) e1, (Object[]) e2);
+        else if (e1 instanceof byte[] && e2 instanceof byte[])
+            eq = Arrays.equals((byte[]) e1, (byte[]) e2);
+        else if (e1 instanceof short[] && e2 instanceof short[])
+            eq = Arrays.equals((short[]) e1, (short[]) e2);
+        else if (e1 instanceof int[] && e2 instanceof int[])
+            eq = Arrays.equals((int[]) e1, (int[]) e2);
+        else if (e1 instanceof long[] && e2 instanceof long[])
+            eq = Arrays.equals((long[]) e1, (long[]) e2);
+        else if (e1 instanceof char[] && e2 instanceof char[])
+            eq = Arrays.equals((char[]) e1, (char[]) e2);
+        else if (e1 instanceof float[] && e2 instanceof float[])
+            eq = Arrays.equals((float[]) e1, (float[]) e2);
+        else if (e1 instanceof double[] && e2 instanceof double[])
+            eq = Arrays.equals((double[]) e1, (double[]) e2);
+        else if (e1 instanceof boolean[] && e2 instanceof boolean[])
+            eq = Arrays.equals((boolean[]) e1, (boolean[]) e2);
+        else
+            eq = e1.equals(e2);
+        return eq;
     }
 }
